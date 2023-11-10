51d5b1cdf456cb718d9ffe41a01964a20aad761b
atipi132 <hugo.pothieux@protonmail.com>
2023-11-10 01:19:01 +0100

    Update build.gradle.kts config file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.ramsesandco.langbot"
         minSdk = 24
-        targetSdk = 33
+         targetSdk = 33
         versionCode = 20
         versionName = "0.2.0"
 
@@ -43,8 +43,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0-alpha03")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0-beta01")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
